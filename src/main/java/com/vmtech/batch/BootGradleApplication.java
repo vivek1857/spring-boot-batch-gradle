0a66e80858602eec906a20929026eb8de34eba45
vivekk1857 <vivekk1857@googlemail.com>
2018-03-11 20:33:24 +0000

    spring batch gradle example

---
+++ src/main/java/com/vmtech/batch/BootGradleApplication.java
@@ -0,0 +1,9 @@
+package com.vmtech.batch;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class BootGradleApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(BootGradleApplication.class, args);
+	}
+}
